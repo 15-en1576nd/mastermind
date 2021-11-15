ec68ea8146b79f497c0b7f8e820c34edb6fe7d37
marnixah <marnix@hagehoeve.com>
2021-11-15 12:53:05 +0100

    feat(Docker): Dockerfile for SSR #2
    feat: use node adapter for SSR
    
    feat: Dockerfile
    
    ci: docker autobuild

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,16 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+
+		adapter: adapter({
+			out: 'build',
+			precompress: false,
+			env: {
+				host: 'HOST',
+				port: 'PORT'
+			}
+		})
 	}
 };
 
