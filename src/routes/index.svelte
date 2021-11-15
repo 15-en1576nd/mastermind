7189ded4f022404381e9969cf5af23dac78123f9
marnixah <marnix@hagehoeve.com>
2021-11-15 23:54:14 +0100

    feat: instructions screen with button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Instructions from '../components/instructions.svelte';
+	let started = false;
+</script>
+
+{#if started}
+	<!-- Game Canvas and stuff here -->
+{:else}
+	<Instructions />
+	<button>Start Game</button>
+{/if}
