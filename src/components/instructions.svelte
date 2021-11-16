ddd9a70d9fb345d07c175516a9a67ad28b936a3b
marnixah <marnix@hagehoeve.com>
2021-11-16 09:17:26 +0100

    feat: instructions screen with button (#22)

---
+++ src/components/instructions.svelte
@@ -0,0 +1,8 @@
+<h1>Mastermind instructions</h1>
+<p>
+	The game is played with two players. The first player is the "mastermind" and the second player is
+	the "guesser". The mastermind chooses a secret code consisting of four colors (red, green, blue,
+	yellow). The guesser then tries to guess the secret code. The guesser has 12 guesses to guess the
+	secret code. The guesser can guess a color by typing in the color's name (red, green, blue,
+	yellow). The guesser can also guess a color by typing in the color's number (1, 2, 3, 4).
+</p>
