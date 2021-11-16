5a4e763e732e1900a9b45e20bba72f3e7c70bed0
marnixah <marnix@hagehoeve.com>
2021-11-16 11:39:07 +0100

    feat: difficulty slider (#25)
    * feat: difficulty enum
    
    * feat: difficultySlider component
    
    * feat: basic start screen

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,27 @@
 <script lang="ts">
+	import type PlayableMastermindGame from '$lib/classes/playableMastermindGame';
+	import SingleplayerMastermindGame from '$lib/classes/singeplayerMastermindGame';
+	import type { Code } from '$lib/types/code';
+	import DifficultySlider from '../components/difficultySlider.svelte';
 	import Instructions from '../components/instructions.svelte';
-	let started = false;
+	let game: PlayableMastermindGame;
+
+	function startGame() {
+		let numberOfRows = 4 + difficulty;
+		game = new SingleplayerMastermindGame(numberOfRows);
+	}
+	let difficulty = 0;
+	let guessText = '';
+	$: guess = guessText.split(',').map(parseInt) as Code;
 </script>
 
-{#if started}
-	<!-- Game Canvas and stuff here -->
+{#if game}
+	<p>{JSON.stringify(game)}</p>
+	<label>Guess</label>
+	<input placeholder="1,1,1,1" />
+	<label>Difficulty</label>
 {:else}
 	<Instructions />
-	<button>Start Game</button>
+	<DifficultySlider bind:value={difficulty} />
+	<button on:click={startGame}>Start Game</button>
 {/if}
