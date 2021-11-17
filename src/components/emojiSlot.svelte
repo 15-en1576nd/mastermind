aa40f7c35fcdc598e4fba681a22f61a6166eda96
Daneric <42703027+DanericNetwork@users.noreply.github.com>
2021-11-17 09:17:08 +0100

    feat: emoji components (#30)

---
+++ src/components/emojiSlot.svelte
@@ -0,0 +1,18 @@
+<div class="slot">
+    <slot></slot>
+</div>
+
+<style>
+    .slot {
+        width: 2rem;
+        height: 2rem;
+        display: flex;
+        text-align: center;
+		border: 1px solid #474554;
+		border-radius: 2px;
+        justify-content: center;
+        background-color: #ACA7CB;
+		box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+		margin: 0 0 1em 0;
+    }
+</style>
