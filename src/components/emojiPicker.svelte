60e0fd9a46f773cb368c80aecd8147bcaa1f6f44
marnixah <marnix@hagehoeve.com>
2021-11-18 10:46:32 +0100

    feat: EmojiPicker components

---
+++ src/components/emojiPicker.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { selectedEmoji } from '$lib/stores';
+	import emojiMap from '$lib/emojimap';
+	import Emoji from './emoji.svelte';
+	import EmojiSlot from './emojiSlot.svelte';
+	import type { CodeDigit } from '$lib/types/code';
+
+	const clickHandler = (emojiIndex: number) => selectedEmoji.set(emojiIndex as CodeDigit);
+</script>
+
+Hey
+<div class="flex">
+	<p>{$selectedEmoji}</p>
+	{#each { length: 8 } as _, i}
+		<EmojiSlot>
+			<button on:click={() => clickHandler(i)}>
+				<Emoji emoji={emojiMap[i]} />
+			</button>
+		</EmojiSlot>
+	{/each}
+</div>
