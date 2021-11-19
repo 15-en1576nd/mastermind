efe0c539a6185a9efe26a83ea2c6aeae7dd12062
marnixah <marnix@hagehoeve.com>
2021-11-19 09:30:04 +0100

    fix: instructions up to date with site

--- src/components/instructions.svelte
+++ src/components/instructions.svelte
@@ -1,8 +1,10 @@
 <h1>Mastermind instructions</h1>
 <p>
 	The game is played with two players. The first player is the "mastermind" and the second player is
-	the "guesser". The mastermind chooses a secret code consisting of four colors (red, green, blue,
-	yellow). The guesser then tries to guess the secret code. The guesser has 12 guesses to guess the
-	secret code. The guesser can guess a color by typing in the color's name (red, green, blue,
-	yellow). The guesser can also guess a color by typing in the color's number (1, 2, 3, 4).
+	the "guesser". The mastermind chooses a secret code consisting of four to eight emojis(depending
+	on the difficulty). The guesser tries to guess the secret code. The guesser has to guess the
+	secret code in 12 turns. Each turn the guesser can change any of the emojis in the secret code.
+	After each guess, the guesser will be informed about the number of correct emojis and the number
+	of correct positions. A green circle indicates a correct position and a red circle indicates a
+	correct emoji.
 </p>
