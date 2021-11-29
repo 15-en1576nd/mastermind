5b0507e15cf85a9714e3cb26d6b3593bee8c60ff
marnixah <marnix@hagehoeve.com>
2021-11-29 16:45:38 +0100

    fix: disable submit button on gameOver (#47)
    * fix: disable submit button when gameOver
    
    * dev: styling on submitButton.svelte

--- src/components/submitButton.svelte
+++ src/components/submitButton.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import { game } from '$lib/stores';
-import Button from './button.svelte';
+	import Button from './button.svelte';
 
 	$: currentIndex = $game.maxGuesses - $game.guessCount - 1;
 </script>
 
 <Button
 	class="bg-blue-500 min-w-sm my-2"
+	disabled={$game.gameOver}
 	on:click={() => {
 		if ($game.gameOver) return;
 		$game.guess($game.rows[currentIndex].code);
 		game.set($game);
-	}}>Guess</Button>
+	}}>Guess</Button
+>
