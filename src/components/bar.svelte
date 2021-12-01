697a7aa5a2f316b1131f4c365d0bc44e280dad8e
marnixah <marnix@hagehoeve.com>
2021-12-01 10:47:51 +0100

    feat: firebase login button

---
+++ src/components/bar.svelte
@@ -0,0 +1,26 @@
+<script>
+import { user } from "$lib/stores";
+
+import Emoji from "./emoji.svelte";
+
+import Login from "./login.svelte";
+
+</script>
+<div class="bg-gray-50 flex justify-between w-full">
+  <Emoji emoji="😃" />
+  {#if $user}
+    <div class="text-gray-700 text-xl font-bold flex flex-row">
+      {$user.displayName}
+      <img class="h-8 w-8 rounded-full" alt="user profile" src="{$user.photoURL}" />
+    </div>
+    {:else}
+    <Login />
+  {/if}
+  
+</div>
+
+<style>
+  div {
+    min-height: 1.5rem;
+  }
+</style>
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,6 +2,8 @@ import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 import type PlayableMastermindGame from './classes/playableMastermindGame';
 import type { CodeDigit } from './types/code';
+import type { User } from 'firebase/auth';
 
 export const selectedEmoji: Writable<CodeDigit> = writable(1);
 export const game: Writable<PlayableMastermindGame> = writable();
+export const user: Writable<User> = writable();
